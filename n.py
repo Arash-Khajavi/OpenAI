6a9ad65503bda4ca0e88b99ab2d513485c252b73
Arash-Khajavi <arash.khajavi.ghk123@gmail.com>
2025-03-24 18:11:35 +0300

    Create n.py

---
+++ n.py
@@ -0,0 +1,100 @@
+import openai                                                                                                                                                                  
+import googletrans                                                                                                                                                             
+from googletrans import Translator                                                                                                                                             
+import requests                                                                                                                                                                
+import json                                                                                                                                                                    
+from flask import Flask,redirect,render_template,request,url_for,send_file,Response,jsonify                                                                                    
+# import cv2                                                                                                                                                                   
+import string                                                                                                                                                                  
+import random
+import os
+l=[]                                                                                                                                                                           
+messages1=[]                                                                                                                                                                   
+iu=[]                                                                                                                                                                          
+j=[]                                                                                                                                                                           
+ou=[]                                                                                                                                                                          
+op=[]                                                                                                                                                                          
+loi=[]                                                                                                                                                                         
+o9i={}                                                                                                                                                                         
+app=Flask(__name__)                                                                                                                                                            
+openai.api_key=os.getenv("chat")
+@app.route("/chatgpt",methods=["POST","GET"])                                                                                                                                  
+def chat_with_gpt(model="gpt-4o-mini", temperature=0.7, max_tokens=900):                                                                                                       
+    if request.method == "POST":                                                                                                                                               
+        user1=request.form.get("user1")                                                                                                                                        
+        language=request.form.get("language")                                                                                                                                  
+        choice=request.form.get("choice")                                                                                                                                      
+        email = request.form.get("email")                                                                                                                                      
+        translator=Translator()                                                                                                                                                
+        print(o9i)                                                                                                                                                             
+        u=user1+f", every single word of your output must be in {language}"                                                                                                    
+        for i,k in o9i.items():                                                                                                                                                
+            if str(u)  == str(i):                                                                                                                                              
+                i=str(str(k.values()).split("assistant")[1].split("'])")[0])                                                                                                   
+                return f"{i},{render_template("hj.html")}"                                                                                                                     
+        # lpo=[]                                                                                                                                                               
+        # r=str(user1)                                                                                                                                                         
+        def generate():                                                                                                                                                        
+          if request.method == "POST":                                                                                                                                         
+            messages1.append({"role": "user", "content": user1})                                                                                                               
+            # o9i["user"]=str(user1)                                                                                                                                           
+            messages1.append({"role": "system", "content": "You are a helpful assistant."})                                                                                    
+            # print(messages1)                                                                                                                                                 
+            # if len(messages1) > 2:                                                                                                                                           
+            loi.append(messages1[-1])                                                                                                                                          
+            loi.append(messages1[-2])                                                                                                                                          
+            response = openai.ChatCompletion.create(                                                                                                                           
+                model=model,                                                                                                                                                   
+                messages=loi,                                                                                                                                                  
+                timeout=30,                                                                                                                                                    
+                temperature=temperature,                                                                                                                                       
+                max_tokens=max_tokens,                                                                                                                                         
+                stream=True                                                                                                                                                    
+            )                                                                                                                                                                  
+            print(messages1[-1],messages1[-2])                                                                                                                                 
+            # o.remove(o[1])                                                                                                                                                   
+            # for chunk in response:                                                                                                                                           
+            #     if chunk.choices[0].delta.get("content"):                                                                                                                    
+            #         yield str(chunk.choices[0].delta.get("content"))                                                                                                         
+                                                                                                                                                                               
+                    # ou.append(chunk.choices[0].delta.get("content",""))                                                                                                      
+                                                                                                                                                                               
+            for chunk in response:                                                                                                                                             
+                     if "choices" in chunk and chunk.choices:                                                                                                                  
+                       text=chunk.choices[0].delta.get("content", "")                                                                                                          
+                       op.append(str(text))                                                                                                                                    
+                       ou.append(str(language))                                                                                                                                
+                       o9i[str(user1)]={"role": "assistant", "content": "".join(op)}                                                                                           
+                       # for o in op:                                                                                                                                          
+                                                                                                                                                                               
+                    # re=translator.translate(str(text),src="auto",dest=str(language)).text                                                                                    
+                    # op.append(str(re))                                                                                                                                       
+                    # print(op)                                                                                                                                                
+                    # oi=translator.translate("".join(op),src="auto",dest="en").text                                                                                           
+                    # iu.append(str(oi))                                                                                                                                       
+                                                                                                                                                                               
+                                                                                                                                                                               
+              # if str(translator.detect("".join(j)).lang) != str(i):                                                                                                          
+                                                                                                                                                                               
+                # messages1.append({"role": "assistant", "content":Response(generate(), content_type="text/event-stream")})                                                    
+                # # oi=[]                                                                                                                                                      
+                # # op=["l","m","p"]                                                                                                                                           
+                # for r in range(len(op)):                                                                                                                                     
+                #     if r != len(op)-1:                                                                                                                                       
+                #      o=op[r],"\n",op[r+1]                                                                                                                                    
+                #      oi.append(o)                                                                                                                                            
+                                                                                                                                                                               
+        op.clear()                                                                                                                                                             
+        translator=Translator()                                                                                                                                                
+        return f"{Response(generate(), content_type="text/event-stream")} {translator.translate("".join(op),src="auto",dest=str(language)).text},{render_template("hj.html")}" 
+    messages1.clear()                                                                                                                                                          
+    return render_template("hj.html")                                                                                                                                          
+            # oi.append(str(o))                                                                                                                                                
+            # print(oi)                                                                                                                                                        
+            # else:                                                                                                                                                            
+            #     print(op[r])                                                                                                                                                 
+            # print(oi)                                                                                                                                                        
+        # return f"{Response(generate(), content_type="text/event-stream")},{oi}"                                                                                              
+        # for i in "".joi                                                                                                                                                      
+if __name__ == "__main__":                                                                                                                                                     
+    app.run(host="0.0.0.0",port=8080,debug=True)                                                                                                                               
